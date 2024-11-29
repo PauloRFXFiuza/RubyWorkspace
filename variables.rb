faf8ff8c585418d3e6ae774af54da1b389a7814f
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-11-29 10:07:24 -0300

    com3

--- variables.rb
+++ variables.rb
@@ -1,4 +1,18 @@
+puts "--- Initiate Ruby ---"
+#Comments
+#Variables
 nm = "Paulo Fiuza"
 ag = 33
 
-puts "#{nm} tem #{ag} anos."
+puts "#{nm} Is #{ag} Years Old.."
+
+#Inputs on Ruby = puts
+puts "What's Your Name?"
+nm2 = gets.chomp 
+#"chomp desconsidera a última tecla digitada - neste caso o enter - ignores the last key typed - in this case enter"
+puts "How Old Are You?"
+ag2 = gets.chomp.to_i #"to_i means conversion string to int variable"
+
+puts "#{nm2} Is #{ag2} Years Old."
+
+puts "--- Done! ---"
